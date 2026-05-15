5a67b64aa5e069ba260f0aa8f57de47f035c30a2
Abhijith T M <abhijithtm861@gmail.com>
2026-05-15 20:57:20 +0530

    corrected base api

--- job-tracker-frontend/src/api/client.js
+++ job-tracker-frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  "https://job-tracker-backend.onrender.com/api/";
+const API_URL =  "https://job-tracking-system-1-c57w.onrender.com/api/";
 
 const client = axios.create({
   baseURL: API_URL,
